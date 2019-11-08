1ed03da63c9b19b79c6084d96c1f7f2cf8945f72
lazyheads99 <lazyheads99@gmail.com>
2019-11-08 17:45:05 +0530

    MP added

--- add.php
+++ add.php
@@ -1,7 +1,18 @@
 <form action="" method="POST" >
-    <input name="name"/>
+    <input name="name" placeholder="name" />
     <input name="sap_id"/>
     <input name="branch"/>
-    <input name="score"/>
+    <select name="score">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
     <button type="submit">ADD</button>
 </form>
---
+++ table.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<table border=\"1\" >";
+        echo "<tr><td>Hello</td><td>World!</td></tr>";
+        echo "<tr><td>Hello</td><td>World!</td></tr>";
+        echo "</table>";
+    ?>
+</body>
+</html>
